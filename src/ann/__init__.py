746e492cac6969195e4177f300a2c22b175225cf
Ramakrishna Sadam <srksrksrksrk2005@gmail.com>
2026-03-04 20:29:09 +0530

    Update __init__.py

--- src/ann/__init__.py
+++ src/ann/__init__.py
@@ -1,6 +1,22 @@
 """
-utils Module
+ANN Module - Implementation of a Multi-Layer Perceptron.
 """
+
+from .neural_network import NeuralNetwork
+from .neural_layer import Linear
+from .activations import ReLU, Sigmoid, Tanh
+from .objective_functions import Cross_Entropy, MSE
+from .optimizers import SGD, Momentum, NAG, RMSProp
 __all__ = [
-    "data_loader"
+    "NeuralNetwork",
+    "Linear",
+    "ReLU",
+    "Sigmoid",
+    "Tanh",
+    "Cross_Entropy",
+    "MSE",
+    "SGD",
+    "Momentum",
+    "NAG",
+    "RMSProp",
 ]
