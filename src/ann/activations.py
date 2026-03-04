e56933d3080b3bb809bbe687ea9c48af0eff34ac
Ramakrishna Sadam <srksrksrksrk2005@gmail.com>
2026-03-04 20:30:17 +0530

    Implement activation functions: ReLU, Sigmoid, Tanh

---
+++ src/ann/activations.py
@@ -0,0 +1,38 @@
+import numpy as np
+"""
+Activation Functions and Their Derivatives
+Implements: ReLU, Sigmoid, Tanh, Softmax
+"""
+class ReLU:
+    def __init__(self):
+        self.output = None
+    def forward(self, Z):
+        self.output = np.maximum(0, Z)
+        return self.output
+
+    def backward(self, grad):
+        return grad * (self.output >= 0).astype(grad.dtype)
+
+
+class Sigmoid:
+    def __init__(self):
+        self.output = None
+
+    def forward(self, Z):
+        Z = np.clip(Z, -50, 50)
+        self.output = 1.0 / (1.0 + np.exp(-Z))
+        return self.output
+
+    def backward(self, grad):
+        return grad * self.output * (1.0 - self.output)
+
+
+class Tanh:
+    def __init__(self):
+        self.output = None
+    def forward(self, Z):
+        self.output = np.tanh(Z)
+        return self.output 
+    
+    def backward(self, grad):
+        return grad * (1 - self.output**2)
