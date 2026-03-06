6d03368bb1b3f10dd7f905cce6ffea4d2a7d1233
Ramakrishna Sadam <srksrksrksrk2005@gmail.com>
2026-03-06 13:45:09 +0530

    Update neural_layer.py

--- src/ann/neural_layer.py
+++ src/ann/neural_layer.py
@@ -2,31 +2,34 @@
 Neural Layer Implementation
 Handles weight initialization, forward pass, and gradient computation
 """
-import numpy as np 
-class Linear:
-    def __init__(self,input_size,output_size,weight_init='random'):
-        self.input = None
-        self.W = None
-        self.b = None
-        self.grad_W = None
-        self.grad_b = None
+import numpy as np
 
-        if weight_init == 'xavier':
+class Linear:
+
+    def __init__(self, input_size, output_size, weight_init="random"):
+
+        if weight_init == "xavier":
             limit = np.sqrt(6 / (input_size + output_size))
             self.W = np.random.uniform(-limit, limit, (input_size, output_size))
         else:
             self.W = np.random.randn(input_size, output_size) * 0.01
+
         self.b = np.zeros((1, output_size))
+
+        self.input = None
+        self.grad_W = None
+        self.grad_b = None
+
     def forward(self, X):
+
         self.input = X
         return X @ self.W + self.b
 
     def backward(self, grad):
 
-        batch_size = self.input.shape[0]
-
-        self.grad_W = (self.input.T @ grad)/ batch_size
-        self.grad_b = np.sum(grad, axis=0, keepdims=True)/ batch_size
+        # NO batch division here
+        self.grad_W = self.input.T @ grad
+        self.grad_b = np.sum(grad, axis=0, keepdims=True)
 
         dX = grad @ self.W.T
 
