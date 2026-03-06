3daa27101a2b91f8052a62aaf98a84ec077f100f
Ramakrishna Sadam <srksrksrksrk2005@gmail.com>
2026-03-06 13:45:34 +0530

    Update objective_functions.py

--- src/ann/objective_functions.py
+++ src/ann/objective_functions.py
@@ -3,6 +3,8 @@ Loss/Objective Functions and Their Derivatives
 Implements: Cross-Entropy, Mean Squared Error (MSE)
 """
 import numpy as np
+
+
 class Cross_Entropy:
 
     def __init__(self):
@@ -12,32 +14,40 @@ class Cross_Entropy:
     def forward(self, logits, y_true):
 
         exp = np.exp(logits - np.max(logits, axis=1, keepdims=True))
-        self.y_pred = exp / np.sum(exp, axis=1, keepdims=True)
+        probs = exp / np.sum(exp, axis=1, keepdims=True)
+
+        self.y_pred = probs
         self.y_true = y_true
 
-        loss = -np.sum(y_true * np.log(self.y_pred + 1e-8)) / y_true.shape[0]
+        loss = -np.sum(y_true * np.log(probs + 1e-8)) / y_true.shape[0]
+
         return loss
 
     def backward(self):
 
-        batch_size = self.y_true.shape[0]
-        return (self.y_pred - self.y_true)/ batch_size
+        batch = self.y_true.shape[0]
+
+        return (self.y_pred - self.y_true) / batch
+
 
 class MSE:
 
     def __init__(self):
+
         self.y_pred = None
         self.y_true = None
+
 
     def forward(self, y_pred, y_true):
 
         self.y_pred = y_pred
         self.y_true = y_true
 
-        return np.mean((y_pred - y_true)**2)
+        return np.mean((y_pred - y_true) ** 2)
+
 
     def backward(self):
 
         batch = self.y_true.shape[0]
 
-        return 2*(self.y_pred - self.y_true)/(batch)
+        return 2 * (self.y_pred - self.y_true) / (batch * self.y_true.shape[1])
