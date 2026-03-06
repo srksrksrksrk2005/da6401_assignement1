a591183badf4bcbb4ac91e35b8023e2b4b2dc5bc
Ramakrishna Sadam <srksrksrksrk2005@gmail.com>
2026-03-06 14:05:16 +0530

    Update neural_layer.py

--- src/ann/neural_layer.py
+++ src/ann/neural_layer.py
@@ -4,6 +4,7 @@ Handles weight initialization, forward pass, and gradient computation
 """
 import numpy as np
 
+
 class Linear:
 
     def __init__(self, input_size, output_size, weight_init="random"):
@@ -20,17 +21,22 @@ class Linear:
         self.grad_W = None
         self.grad_b = None
 
+
     def forward(self, X):
 
         self.input = X
         return X @ self.W + self.b
 
+
     def backward(self, grad):
 
-        # NO batch division here
-        self.grad_W = (self.input.T @ grad)/self.input.shape[0]
-        self.grad_b = np.sum(grad, axis=0, keepdims=True)/self.input.shape[0]
+        # gradients w.r.t weights
+        self.grad_W = self.input.T @ grad
 
+        # gradients w.r.t bias
+        self.grad_b = np.sum(grad, axis=0, keepdims=True)
+
+        # gradient to previous layer
         dX = grad @ self.W.T
 
         return dX
