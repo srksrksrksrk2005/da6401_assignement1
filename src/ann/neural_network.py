263621689e023dd4010644b45a3c0a2f871158e8
Ramakrishna Sadam <srksrksrksrk2005@gmail.com>
2026-03-06 13:47:58 +0530

    Update neural_network.py

--- src/ann/neural_network.py
+++ src/ann/neural_network.py
@@ -111,8 +111,7 @@ class NeuralNetwork:
             if hasattr(layer, "W"):
                 grad_W_list.append(layer.grad_W)
                 grad_b_list.append(layer.grad_b)
-        grad_W_list.reverse()
-        grad_b_list.reverse()
+        
         self.grad_W = np.empty(len(grad_W_list), dtype=object)
         self.grad_b = np.empty(len(grad_b_list), dtype=object)
 
