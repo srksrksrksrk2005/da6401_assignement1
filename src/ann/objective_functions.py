c549806b8b188cd1b6564847402bd4e4d05ff432
Ramakrishna Sadam <srksrksrksrk2005@gmail.com>
2026-03-07 11:15:12 +0530

    Update objective_functions.py

--- src/ann/objective_functions.py
+++ src/ann/objective_functions.py
@@ -11,7 +11,6 @@ class Cross_Entropy:
         self.y_pred = None
         self.y_true = None
 
-
     def forward(self, logits, y_true):
         exp = np.exp(logits - np.max(logits, axis=1, keepdims=True))
         probs = exp / np.sum(exp, axis=1, keepdims=True)
@@ -20,9 +19,7 @@ class Cross_Entropy:
         loss = -np.sum(y_true * np.log(probs + 1e-12)) / logits.shape[0]
         return loss
 
-
     def backward(self):
-
         return (self.y_pred - self.y_true)/self.y_true.shape[0]
 
 
@@ -33,16 +30,12 @@ class MSE:
         self.y_pred = None
         self.y_true = None
 
-
     def forward(self, y_pred, y_true):
 
         self.y_pred = y_pred
         self.y_true = y_true
-
         loss = np.sum((y_pred - y_true) ** 2)
-
         return loss
-
 
     def backward(self):
         batch = self.y_true.shape[0]
